16e48be42d0c7fc2fe915b7224645e557268a2ef
moataz_emad_naeem <moatazemad772@gmail.com>
2022-03-10 17:33:59 +0200

    Edited auth middleware v2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,9 +2,6 @@ const {NotAuth}=require('../errorclasses/notauth')
 const jwt =require('jsonwebtoken') 
 const Auth=(req,res,next)=>{
 
-    if(!req.session?.jwt){
-       return next(new NotAuth('You are not authenticated')) 
-    }
     if(req.headers.authentication){
         try{
             const payload= jwt.verify(req.headers.authentication,process.env.JWT_KEY)
@@ -13,6 +10,9 @@ const Auth=(req,res,next)=>{
             return next(new NotAuth('You are not authenticated')) 
           }
         return next()
+    }
+    if(!req.session?.jwt){
+       return next(new NotAuth('You are not authenticated')) 
     }
     try{
           const payload= jwt.verify(req.session.jwt,process.env.JWT_KEY)
