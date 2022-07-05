2cf4e74fb87f371f1b370019594a3e7beee41fbe
moataz_emad_naeem <moatazemad772@gmail.com>
2022-07-05 22:13:16 +0200

    added generic message

--- middlewares/handelError.js
+++ middlewares/handelError.js
@@ -2,7 +2,16 @@ const handelerr=(err,req,res,next)=>{
     let transformed;
     let errtransformed;
     transformed=err.summary()
-    errtransformed={errors:transformed,statusCode:err.statusCode,status:false}
+    let generic_message='';
+    transformed.forEach((item,index)=>{
+        if(transformed.length-1===index){
+            generic_message+=`(${item.field}): ${item.message}`
+
+        }else{
+            generic_message+=`(${item.field}): ${item.message} And `
+        }
+    })
+    errtransformed={errors:transformed,statusCode:err.statusCode,status:false,generic_message}
     return res.status(err.statusCode).send(errtransformed)
 }
 module.exports={handelerr}
