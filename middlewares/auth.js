6f90c0bb9d4c86969dbc27e515ea5de57482b7b3
moataz_emad_naeem <moatazemad772@gmail.com>
2022-09-03 22:43:20 +0200

    fixed auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,18 +4,7 @@ const {user}=require('../models/BaseModel')
 const {BadReqErr} =require('../errorclasses/badReq')
 const {roles}=require('../types/roles')
 const Auth=async(req,res,next)=>{
-      try{
-        const User=await user.findById(req.currentUser.id)
-        if(!User){
-          return next(new NotAuth('You are not authenticated')) 
-        }
-        if(User.roles!==roles.NORMALSELLER){
-          return next(new NotAuth('You are not authenticated')) 
-        }
-      }catch(err){
-        return next(new BadReqErr(err.message)) 
-      }
-    
+      
     if(req.headers.authentication){
         try{
             const payload= jwt.verify(req.headers.authentication,process.env.JWT_KEY)
