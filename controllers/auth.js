a6b9fa2749b4120ef65cf6eec1ad078f29622a3d
moataz_emad_naeem <moatazemad772@gmail.com>
2022-09-24 18:50:14 +0200

    edited the text on h1 tag

--- controllers/auth.js
+++ controllers/auth.js
@@ -152,7 +152,7 @@ module.exports={
             if(User){
                 User.IsValid=true;
                 await User.save()
-                res.send('<h1>Done Verifying.</h1>')
+                res.send('<h1>Done Verifying Please Return Back To The App.</h1>')
             }
             else{
                 throw new notfound('can not find the user')
