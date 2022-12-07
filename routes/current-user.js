40e99be072155373002b9482e95fe21b5a6d07fb
moataz_emad_naeem <moatazemad772@gmail.com>
2022-12-08 00:51:42 +0200

    changed current from get to post

--- routes/current-user.js
+++ routes/current-user.js
@@ -3,5 +3,5 @@ const {current}=require('../controllers/auth')
 const {ExtractPayload}=require('../middlewares/extractPayload')
 const router=express.Router()
 
-router.get('/current-user',ExtractPayload,current)
+router.post('/current-user',ExtractPayload,current)
 module.exports={current:router}
