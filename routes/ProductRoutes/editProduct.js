ab94505b12dcf2d27e12f83c8bb41fbb424b5c3c
moataz_emad_naeem <moatazemad772@gmail.com>
2023-01-22 19:14:48 +0200

    fixed quantity

--- routes/ProductRoutes/editProduct.js
+++ routes/ProductRoutes/editProduct.js
@@ -11,7 +11,7 @@ router.put('/edit_product',Auth,IsSeller,
     body('title').optional().isLength({min:2,max:255}).withMessage('title must be at least 3 chars long and 255 max'),
     body('price').optional().isNumeric().isFloat({min:0}).withMessage('price must be valid'),
     body('desc').optional().isLength({min:2,max:2000}).withMessage('desc must be at least 3 chars and 2000 max'),
-    body('quantity').optional().isLength({min:1}).withMessage('quantity should be at least one product'),
+    body('quantity').optional().isNumeric().isFloat({min:1}).withMessage('quantity should be at least one product'),
     body('factory').optional().isLength({min:2,max:255}).withMessage('factory must be at least 3 chars and 255 max'),
     body('warranty').optional().isLength({min:2,max:255}).withMessage('warranty must be at least 3 chars long and 255 max')
 ],
